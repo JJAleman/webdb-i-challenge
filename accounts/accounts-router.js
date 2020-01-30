6e84842676a31c852f1c2c7b0dbf669ae9d8186d
Juan Aleman <juan.j.aleman@gmail.com>
2020-01-29 21:22:50 -0600

    added POST

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -1,28 +1,37 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('../data/dbConfig.js');
+const db = require("../data/dbConfig.js");
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try {
-        const accounts = await db('accounts');
-        console.log(accounts);
-        res.status(200).json({message: 'Accounts retrieved'})
-    } catch(err){
-        res.status(500).json({message: 'Can not /GET Accounts'});
-    }
+router.get("/", async (req, res) => {
+  try {
+    const accounts = await db("accounts");
+    console.log(accounts);
+    res.status(200).json({ message: "Accounts retrieved", accounts });
+  } catch (err) {
+    res.status(500).json({ message: "Can not /GET Accounts" });
+  }
 });
 
-router.get('/:id', async (req, res) => {
-    const { id } = req.params;
-    try{
-        const account = await db('account')
-        .where('id', id)
-        res.status(200).json({message: `Retrieved account`, account})
-    } catch(err){
-        res.status(500).json({message: 'Failed to get post'});
-    }
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const account = await db("accounts").where("id", id);
+    res.json(account);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to get post" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const postAccount = req.body;
+  try {
+    const account = await db("accounts").insert(postAccount);
+    res.status(201).json({message:'Added account', account});
+  } catch (err) {
+    res.status(500).json({ message: "Failed to post account" });
+  }
 });
 
 module.exports = router;
